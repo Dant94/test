05213920a2e12b2d98eb938edfa15e8c0acc9032
Dant <antimonov94@gmail.com>
2019-09-12 13:20:36 +0500

    First commit

---
+++ script.js
@@ -0,0 +1,35 @@
+"use strict";
+
+
+// let person = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+// };
+// console.log(person["name"]);
+
+// let arr = ['lum.png', 'orange.jpg', 'apple.bmp'];
+// console.log(arr[2]);
+
+// // alert("Hello World!");
+
+// // let answer = confirm("Are you here");
+// // console.log(answer);
+
+// let answer = prompt("Есть ли вам 18?", "Да");
+// console.log(typeof(arr));
+
+let incr = 10;
+let decr = 10;
+
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+console.log("2" === 2);
+
+let isChecked = true;
+let isClose = false;
+
+console.log(isChecked || !isClose);
